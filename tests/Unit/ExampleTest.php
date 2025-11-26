d65a15d31accdf5e107b9c94d38e38b972aaae3a
Khien <kcp@cct-technology.com>
2025-11-26 10:34:20 +0700

    .

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class ExampleTest extends TestCase
+class ExampleTest extends TestCase
 {
     public function testTrue(): void
     {
