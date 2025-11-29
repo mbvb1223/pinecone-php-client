57ffa7fbd41ba0467a93844aaa37a19f1b364888
Khien <kcp@cct-technology.com>
2025-11-29 09:22:21 +0700

    [Task-9] Name must consist of lower case alphanumeric characters or -

--- tests/Integration/IndexNamespaceTest.php
+++ tests/Integration/IndexNamespaceTest.php
@@ -10,7 +10,7 @@ class IndexNamespaceTest extends BaseIntegrationTestCase
 {
     public function testIndexNamespaceOperations(): void
     {
-        $indexName = 'test-IndexNamespaceTest';
+        $indexName = 'test-indexnamespacetest';
 
         $this->pinecone->createIndex($indexName, [
             'dimension' => 1024,
